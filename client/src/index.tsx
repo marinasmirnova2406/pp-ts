cde9882869ec15dc6ff25fb8955090ef239e487a
marinasmirnova2406 <marinasmirnova2406@gmail.com>
2024-10-29 17:15:23 +0100

    Structuring styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
+import './styles/main.scss';
 
 const rootElement = document.getElementById("root");
 
