c043a138fb3ca558ae55135534fad5e4147ec5dc
marinasmirnova2406 <marinasmirnova2406@gmail.com>
2024-11-11 16:06:37 +0100

    Fixed eslint

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'resolve-url-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
